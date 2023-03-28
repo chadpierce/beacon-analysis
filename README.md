0fe454e1f444733d4ab73cddbed5d8346950f565
Chad Pierce <chadpierce@users.noreply.github.com>
2023-03-27 22:48:06 -0500

    new flags, remove dup timestamps, plus other stuff

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # C2 Beacon Analysis Tools
 
-This repo contains a few utilities that take logs from any data source you'd like in .csv format. The intent is for this to be generic
+This repo contains a few utilities that take logs from any data source you'd like in some delimited flat file.  
+The intent is for this to be generic and work against any log source.  
 
 Currently, the following files exist:
 
@@ -16,21 +17,24 @@ For each version, the input file must include:
     - bytes received
     - bytes sent
 
-Currently, the Go version is the most interesting to me because the use of goroutines speeds up analysis quite a bit on test data.  
-YMMV on production data that is huge. The lack of Python dependencies and portable binaries is a bonus. 
+Currently, the Go version is the most interesting to me because the use of goroutines speeds up analysis quite a bit.  
+The lack of Python dependencies and portable binaries is a bonus.  
 
 ## Usage
 
-Default values are currently in play, but options can be modified using the arguments listed below:
+Default values are currently in play, but options can be modified using the arguments listed below.  
+There are default parameters that work for me, but most can be modified by passing arguments.  
 
 ```
 Usage of program:
+Usage of program:
   -B    do not use bytes sent/received in analysis
   -D    use DNS Log CSV Inputs (no size analysis)
+  -O    write output to inputfilename.out
   -P    use Proxy Log CSV Inputs
   -S float
         minimum score threshold (default 0.5)
-  -X    enable debug mode for extra output (TODO)
+  -X    [TODO] enable debug mode for extra output
   -cd int
         csv column for destination (default 7)
   -cr int
@@ -41,6 +45,8 @@ Usage of program:
         csv column for timestamp (default 0)
   -cx int
         csv column for bytes sent (default 12)
+  -d string
+        input csv delimiter (put in quotes: ';' (default ",")
   -h    display help
   -i string
         input csv filename
@@ -62,7 +68,12 @@ Usage of program:
 
 ## TODO
 
+- Tune default scoring
+- Tune algorithm outputs
 - Add customizable thresholds for the 4 scoring systems
-- Add input arguments with optional values for thresholds
-- Create a DNS log generator
+- Add input arguments with optional values for those thresholds
 - If this is useful, write more documentation (if it wasn't useful it was a fun experiement)
+- ignore input lines that don't start with proper date format? - print warning to screen
+- add start/stop time for session (and duration) to output, potentially use for scoring
+- debug mode that prints all datapoints for each pair
+- Create a DNS log generator
--- beacon_finder.go
+++ beacon_finder.go
@@ -1,6 +1,7 @@
 package main
 
 /*	beacon_finder.go
+ *  github.com/chadpierce/beacon_analysis
  *
  *	THIS SCRIPT IS PROVIDED "AS IS" WITH NO WARRANTIES OR GUARANTEES OF ANY
  *	KIND, INCLUDING BUT NOT LIMITED TO MERCHANTABILITY AND/OR FITNESS FOR A
@@ -16,15 +17,11 @@ package main
  *	- If beacon traffic ==> uniform distribution and small Median Absolute Deviation of time deltas
  *	- If user traffic ==> skewed distribution and large Median Absolute Deviation of time deltas
  *
- *	Rudimentary data size analysis has been added.
+ *	Rudimentary MAD based data size analysis has been added.
  *
- *	At the time of this writing, this code is completely written by Bing Chat AI.
- *	Source code from RITA_pcap was fed to the AI, and then it was instructed to re-write
- *	the same logic in go using only native libraries (some tweaking was required).
- *	Some additional features have been added manually.
- *
- *
- * 	This is documentation also written by Bing Chat:
+ *	Source code from RITA_pcap.ipynb was fed to a chat AI, then the bot was instructed to re-write
+ *	the same logic in Go using only native libraries (some tweaking was required to get this working).
+ *	Several additional features have been added manually.
  *
  * 	The calculation of each individual score can be tuned to account for variances in real-world traffic by adjusting the
  *		parameters used in their respective calculations.
@@ -56,11 +53,13 @@ import (
 	"time"
 )
 
-// Defining arguments
+// arguments
 type Options struct {
 	Help            bool
 	InputFile       string
 	OutputFile      string
+	OutputDefault   bool
+	Comma           string
 	ColumnTime      int
 	ColumnSource    int
 	ColumnDest      int
@@ -79,7 +78,7 @@ type Options struct {
 	Debug           bool
 }
 
-// Record represents a row in the CSV file
+// represents a row in the CSV file
 type Record struct {
 	Timestamp     time.Time
 	Src           string
@@ -88,7 +87,7 @@ type Record struct {
 	BytesReceived int
 }
 
-// GroupedRecord represents a group of records with the same source and destination
+// represents a group of records with the same source and destination
 type GroupedRecord struct {
 	Src           string
 	Dst           string
@@ -98,7 +97,7 @@ type GroupedRecord struct {
 	ReceivedSizes []int
 }
 
-// ScoredRecord represents a grouped record with calculated scores
+// represents a grouped record with calculated scores
 type ScoredRecord struct {
 	Src            string
 	Dst            string
@@ -110,59 +109,11 @@ type ScoredRecord struct {
 }
 
 func main() {
-	var opts Options
-	flag.BoolVar(&opts.Help, "h", false, "display help")
-	flag.StringVar(&opts.InputFile, "i", "", "input csv filename")
-	flag.StringVar(&opts.OutputFile, "o", "", "write output to given filename ")
-	flag.IntVar(&opts.MinConnCount, "m", 36, "minimum number of connections threshold")
-	flag.IntVar(&opts.MaxSources, "s", 5, "maximum number of sources for destination threshold")
-	flag.Float64Var(&opts.MinScore, "S", .500, "minimum score threshold")
-	flag.IntVar(&opts.ColumnTime, "ct", 0, "csv column for timestamp (default 0)")
-	flag.IntVar(&opts.ColumnSource, "cs", 2, "csv column for source")
-	flag.IntVar(&opts.ColumnDest, "cd", 7, "csv column for destination")
-	flag.IntVar(&opts.ColumnByteRecv, "cr", 11, "csv column for bytes recevied")
-	flag.IntVar(&opts.ColumnByteSent, "cx", 12, "csv column for bytes sent")
-	flag.Float64Var(&opts.WeightSkew, "ws", 1.0, "weight value for skew score")
-	flag.Float64Var(&opts.WeightMadm, "wm", 1.0, "weight value for MADM score")
-	flag.Float64Var(&opts.WeightConnCount, "wc", 1.0, "weight value connection count score")
-	flag.Float64Var(&opts.WeightSize, "wz", 1.0, "weight value for data size score")
-	flag.BoolVar(&opts.InputProxy, "P", false, "use Proxy Log CSV Inputs")
-	flag.BoolVar(&opts.InputDNS, "D", false, "use DNS Log CSV Inputs (no size analysis)")
-	flag.BoolVar(&opts.InputDNS, "B", false, "do not use bytes sent/received in analysis")
-	flag.BoolVar(&opts.InputDNS, "X", false, "enable debug mode for extra output (TODO)")
-	flag.Parse()
-	// Check if -h flag is provided
-	if opts.Help {
-		fmt.Println("Usage of program:")
-		flag.PrintDefaults()
-		os.Exit(0)
-	}
-	if opts.InputFile == "" {
-		fmt.Println("Must supply input file (-i filename.csv)")
-		os.Exit(0)
-	}
 
-	if opts.InputProxy && opts.InputDNS {
-		fmt.Println("ERROR: cannot use both -P and -D")
-		os.Exit(0)
-	} else if opts.InputProxy && !opts.InputDNS {
-		fmt.Println("INFO: Proxy mode selected")
-		opts.ColumnTime = 0
-		opts.ColumnSource = 2
-		opts.ColumnDest = 7
-		opts.ColumnByteSent = 11
-		opts.ColumnByteRecv = 12
-	} else if !opts.InputProxy && opts.InputDNS {
-		fmt.Println("INFO: DNS mode selected")
-		opts.ColumnTime = 0
-		opts.ColumnSource = 1
-		opts.ColumnDest = 2
-		opts.ColumnByteSent = -1
-		opts.ColumnByteRecv = -1
-		opts.WeightSize = 0
-		opts.NoBytes = true
-	}
+	opts := getOptions()
 
+	// TODO check for single char input ...although anything past the first char gets ignored anyway?
+	commaRune := []rune(opts.Comma)[0] // convert string to rune
 	timeCol := opts.ColumnTime
 	srcCol := opts.ColumnSource
 	dstCol := opts.ColumnDest
@@ -176,7 +127,10 @@ func main() {
 	defer file.Close()
 
 	reader := csv.NewReader(file)
+	reader.Comma = commaRune // csv separator
 	var records []Record
+
+	log.Println("INFO: starting...")
 
 	for {
 		row, err := reader.Read()
@@ -184,28 +138,41 @@ func main() {
 			break
 		}
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal(err) // TODO handle this differently?
+			//log.Println("WARNING: ", err)  // maybe like this
+			//continue
 		}
 
-		// Skip rows where source or destination is "-"
+		// skip rows where source or destination is "-"  // TODO make this optional?
 		if row[srcCol] == "-" || row[dstCol] == "-" {
 			continue
 		}
-		// Parse timestamp from first column
+
+		// parse timestamp from first column
 		timestamp, err := time.Parse("2006-01-02-15:04:05", row[timeCol])
 		if err != nil {
-			log.Fatal(err)
+			//log.Fatal(err)  // throw warning and skip line - not sure if good idea?
+			log.Println("WARNING: ", err)
+			continue
 		}
 
-		// Parse bytes sent and received from their respective columns
-		bytesSent, err := strconv.Atoi(row[bytesSentCol])
-		if err != nil {
-			log.Fatal(err)
-		}
+		// if NoBytes flag was passed, set to 0 - otherwise get values from csv
+		var bytesSent int
+		var bytesReceived int
+		if opts.NoBytes {
+			bytesSent = 0
+			bytesReceived = 0
+		} else {
+			// parse bytes sent and received from their respective columns
+			bytesSent, err = strconv.Atoi(row[bytesSentCol])
+			if err != nil {
+				log.Fatal(err) // TODO maybe warn but continue?
+			}
 
-		bytesReceived, err := strconv.Atoi(row[bytesReceivedCol])
-		if err != nil {
-			log.Fatal(err)
+			bytesReceived, err = strconv.Atoi(row[bytesReceivedCol])
+			if err != nil {
+				log.Fatal(err) // TODO maybe warn but continue?
+			}
 		}
 
 		record := Record{
@@ -219,14 +186,15 @@ func main() {
 		records = append(records, record)
 	}
 
-	// Sort records by timestamp in ascending order
+	// sort records by timestamp in ascending order
 	sort.Slice(records, func(i, j int) bool {
 		return records[i].Timestamp.Before(records[j].Timestamp)
 	})
 
-	// Group records by source and destination
+	// group records by source and destination, ignoring duplicate timestamps
 	groupedRecords := groupRecords(records)
-	// Remove rows with Popular destinations
+
+	// remove rows with popular destinations
 	groupedRecords = removePopularDestinations(groupedRecords, opts.MaxSources)
 
 	var scoredRecords []ScoredRecord
@@ -285,15 +253,19 @@ func main() {
 				sizeScore = 0
 			}
 
-			// weights for each score can be modified here
+			// weights for each sub-score
 			skewWeight := opts.WeightSkew
 			madmWeight := opts.WeightMadm
 			connCountWeight := opts.WeightConnCount
 			sizeWeight := opts.WeightSize
+			if opts.NoBytes {
+				sizeWeight = 0
+			}
 
 			scoreVal := (skewWeight*skewScoreVal + madmWeight*madmScoreVal + connCountWeight*connCountScoreVal +
 				sizeWeight*sizeScore) / (skewWeight + madmWeight + connCountWeight + sizeWeight)
-			//scoreVal := (skewScoreVal + madmScoreVal + connCountScoreVal + sizeScore) / 4
+			// unweighted scoring:
+			// scoreVal := (skewScoreVal + madmScoreVal + connCountScoreVal + sizeScore) / 4
 
 			scoredRecord := ScoredRecord{
 				Src:            groupedRecord.Src,
@@ -322,22 +294,129 @@ func main() {
 		scoredRecords = append(scoredRecords, scoredRecord)
 	}
 
-	// Sort scored records by score in descending order
+	// sort scored records by score in descending order
 	sort.Slice(scoredRecords, func(i, j int) bool {
 		return scoredRecords[i].Score > scoredRecords[j].Score
 	})
 
-	// Print scored records
-	writeOutput(scoredRecords, opts.OutputFile)
-	// for _, scoredRecord := range scoredRecords {
-	// 	fmt.Printf("%s -> %s | SCORE: %.3f | (skew: %.3f) (madm: %.3f) (connCount: %.3f) (size: %.3f)\n", scoredRecord.Src,
-	//		scoredRecord.Dst, scoredRecord.Score, scoredRecord.SkewScore, scoredRecord.MadmScore, scoredRecord.ConnCountScore,
-	//		scoredRecord.SizeScore)
-	// }
+	// print scored records
+	writeOutput(scoredRecords, opts.OutputFile, opts.NoBytes)
 }
 
-// print scored recordsoutput, and write to file if needed
-func writeOutput(scoredRecords []ScoredRecord, outputFile string) {
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
+}
+
+func getOptions() Options {
+	var opts Options
+	flag.BoolVar(&opts.Help, "h", false, "display help")
+	flag.StringVar(&opts.InputFile, "i", "", "input csv filename")
+	flag.StringVar(&opts.OutputFile, "o", "", "write output to given filename")
+	flag.BoolVar(&opts.OutputDefault, "O", false, "write output to inputfilename.out")
+	flag.StringVar(&opts.Comma, "d", ",", "input csv delimiter (put in quotes: ';'")
+	flag.IntVar(&opts.MinConnCount, "m", 36, "minimum number of connections threshold")
+	flag.IntVar(&opts.MaxSources, "s", 5, "maximum number of sources for destination threshold")
+	flag.Float64Var(&opts.MinScore, "S", .500, "minimum score threshold")
+	flag.IntVar(&opts.ColumnTime, "ct", 0, "csv column for timestamp (default 0)")
+	flag.IntVar(&opts.ColumnSource, "cs", 2, "csv column for source")
+	flag.IntVar(&opts.ColumnDest, "cd", 7, "csv column for destination")
+	flag.IntVar(&opts.ColumnByteRecv, "cr", 11, "csv column for bytes recevied")
+	flag.IntVar(&opts.ColumnByteSent, "cx", 12, "csv column for bytes sent")
+	flag.Float64Var(&opts.WeightSkew, "ws", 1.0, "weight value for skew score")
+	flag.Float64Var(&opts.WeightMadm, "wm", 1.0, "weight value for MADM score")
+	flag.Float64Var(&opts.WeightConnCount, "wc", 1.0, "weight value connection count score")
+	flag.Float64Var(&opts.WeightSize, "wz", 1.0, "weight value for data size score")
+	flag.BoolVar(&opts.InputProxy, "P", false, "use Proxy Log CSV Inputs")
+	flag.BoolVar(&opts.InputDNS, "D", false, "use DNS Log CSV Inputs (no size analysis)")
+	flag.BoolVar(&opts.NoBytes, "B", false, "do not use bytes sent/received in analysis")
+	flag.BoolVar(&opts.Debug, "X", false, "[TODO] enable debug mode for extra output") // TODO
+	flag.Parse()
+	// check if -h flag is passed
+	if opts.Help {
+		fmt.Println("Usage of program:")
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+	if opts.InputFile == "" {
+		log.Println("ERROR: Must supply input file (-i filename.csv)")
+		os.Exit(0)
+	}
+	// if output file flag is passed, make sure it doesn't match input file
+	if isFlagPassed("o") && isFlagPassed("O") {
+		log.Println("ERROR: Cannot specify both -o and -O")
+		os.Exit(0)
+	}
+	if isFlagPassed("o") {
+		if opts.InputFile == opts.OutputFile {
+			log.Println("ERROR: Input and Output files cannot have the same name")
+			os.Exit(0)
+		}
+	}
+	if opts.OutputDefault {
+		outFile := opts.InputFile + ".out"
+		log.Printf("INFO: output will be written to: %s\n", outFile)
+		opts.OutputFile = outFile
+	}
+	// isFlagPassed is used to override defaults if presets -P or -D are used
+	if opts.InputProxy && opts.InputDNS {
+		log.Println("ERROR: cannot use both -P and -D")
+		os.Exit(0)
+	} else if opts.InputProxy && !opts.InputDNS {
+		log.Println("INFO: Proxy mode selected")
+		if !isFlagPassed("ct") {
+			opts.ColumnTime = 0
+		}
+		if !isFlagPassed("cs") {
+			opts.ColumnSource = 2
+		}
+		if !isFlagPassed("cd") {
+			opts.ColumnDest = 7
+		}
+		if !isFlagPassed("cx") {
+			opts.ColumnByteSent = 11
+		}
+		if !isFlagPassed("cr") {
+			opts.ColumnByteRecv = 12
+		}
+		if !isFlagPassed("d") {
+			opts.Comma = " "
+		}
+	} else if !opts.InputProxy && opts.InputDNS {
+		log.Println("INFO: DNS mode selected")
+		if !isFlagPassed("ct") {
+			opts.ColumnTime = 0
+		}
+		if !isFlagPassed("cs") {
+			opts.ColumnSource = 1
+		}
+		if !isFlagPassed("cd") {
+			opts.ColumnDest = 2
+		}
+		if !isFlagPassed("cx") {
+			opts.ColumnByteSent = -1
+		}
+		if !isFlagPassed("cr") {
+			opts.ColumnByteRecv = -1
+		}
+		if !isFlagPassed("wz") {
+			opts.WeightSize = 0
+		}
+		if !isFlagPassed("B") {
+			opts.NoBytes = true
+		}
+	}
+
+	return opts
+}
+
+// print scored records output, and write to file if needed
+func writeOutput(scoredRecords []ScoredRecord, outputFile string, noBytes bool) {
 	var file *os.File
 	var err error
 	if outputFile != "" {
@@ -349,20 +428,33 @@ func writeOutput(scoredRecords []ScoredRecord, outputFile string) {
 	}
 
 	for _, scoredRecord := range scoredRecords {
-		output := fmt.Sprintf("%s -> %s | SCORE: %.3f | (skew: %.3f) (madm: %.3f) (connCount: %.3f) (size: %.3f)\n",
-			scoredRecord.Src, scoredRecord.Dst, scoredRecord.Score, scoredRecord.SkewScore, scoredRecord.MadmScore,
-			scoredRecord.ConnCountScore, scoredRecord.SizeScore)
-		fmt.Print(output)
+		var output string
+		if noBytes {
+			output = fmt.Sprintf("%s -> %s | SCORE: %.3f | (skew: %.3f) (madm: %.3f) (connCount: %.3f) (size: -)\n",
+				scoredRecord.Src, scoredRecord.Dst, scoredRecord.Score, scoredRecord.SkewScore, scoredRecord.MadmScore,
+				scoredRecord.ConnCountScore)
+		} else {
+			output = fmt.Sprintf("%s -> %s | SCORE: %.3f | (skew: %.3f) (madm: %.3f) (connCount: %.3f) (size: %.3f)\n",
+				scoredRecord.Src, scoredRecord.Dst, scoredRecord.Score, scoredRecord.SkewScore, scoredRecord.MadmScore,
+				scoredRecord.ConnCountScore, scoredRecord.SizeScore)
+		}
+		// print to file if output filename exists, or print to console
 		if outputFile != "" {
+			log.Println("INFO: writing to file: ", outputFile)
 			_, err := file.WriteString(output)
 			if err != nil {
 				log.Fatal(err)
 			}
+		} else {
+			log.Println("INFO: finished")
+			fmt.Print(output)
 		}
 	}
 }
 
-// groupRecords groups records by source and destination
+// groups records by source and destination, removing rows with duplicate timestamps,
+// keeping the highest byte value.
+// TODO revisit this methodology
 func groupRecords(records []Record) []GroupedRecord {
 	groupsMap := make(map[string]GroupedRecord)
 
@@ -382,9 +474,25 @@ func groupRecords(records []Record) []GroupedRecord {
 			groupsMap[key] = groupedRecord
 		}
 
-		groupedRecord.Times = append(groupedRecord.Times, record.Timestamp)
-		groupedRecord.SentSizes = append(groupedRecord.SentSizes, record.BytesSent)
-		groupedRecord.ReceivedSizes = append(groupedRecord.ReceivedSizes, record.BytesReceived)
+		found := false
+		for i, t := range groupedRecord.Times {
+			if t == record.Timestamp {
+				found = true
+				if record.BytesSent > groupedRecord.SentSizes[i] {
+					groupedRecord.SentSizes[i] = record.BytesSent
+				}
+				if record.BytesReceived > groupedRecord.ReceivedSizes[i] {
+					groupedRecord.ReceivedSizes[i] = record.BytesReceived
+				}
+				break
+			}
+		}
+
+		if !found {
+			groupedRecord.Times = append(groupedRecord.Times, record.Timestamp)
+			groupedRecord.SentSizes = append(groupedRecord.SentSizes, record.BytesSent)
+			groupedRecord.ReceivedSizes = append(groupedRecord.ReceivedSizes, record.BytesReceived)
+		}
 
 		groupsMap[key] = groupedRecord
 	}
@@ -398,10 +506,10 @@ func groupRecords(records []Record) []GroupedRecord {
 }
 
 func removePopularDestinations(groupedRecords []GroupedRecord, maxDest int) []GroupedRecord {
-	// Create a map to keep track of the number of unique sources for each destination
+	// create a map to keep track of the number of unique sources for each destination
 	destinationCount := make(map[string]map[string]bool)
 
-	// Iterate over the groupedRecords to count the number of unique sources for each destination
+	// iterate over the groupedRecords to count the number of unique sources for each destination
 	for _, record := range groupedRecords {
 		if _, ok := destinationCount[record.Dst]; !ok {
 			destinationCount[record.Dst] = make(map[string]bool)
@@ -409,10 +517,10 @@ func removePopularDestinations(groupedRecords []GroupedRecord, maxDest int) []Gr
 		destinationCount[record.Dst][record.Src] = true
 	}
 
-	// Create a new slice to store the filtered groupedRecords
+	// create a new slice to store the filtered groupedRecords
 	var filteredGroupedRecords []GroupedRecord
 
-	// Iterate over the groupedRecords and only add records where the destination has 5 or fewer unique sources
+	// iterate over the groupedRecords and only add records where the destination has maxDest or fewer unique sources
 	for _, record := range groupedRecords {
 		if len(destinationCount[record.Dst]) <= maxDest {
 			filteredGroupedRecords = append(filteredGroupedRecords, record)
@@ -431,7 +539,7 @@ func percentile(deltas []float64, p float64) float64 {
 	return deltas[int(index)]
 }
 
-// madm calculates the median absolute deviation of the given slice of float64 values
+// calculates the median absolute deviation of the given slice of float64 values
 func madm(deltas []float64) float64 {
 	medianDelta := median(deltas)
 	absDeltas := make([]float64, len(deltas))
@@ -441,7 +549,7 @@ func madm(deltas []float64) float64 {
 	return median(absDeltas)
 }
 
-// median calculates the median of the given slice of float64 values
+// calculates the median of the given slice of float64 values
 func median(deltas []float64) float64 {
 	sort.Float64s(deltas)
 	if len(deltas)%2 == 0 {
@@ -450,7 +558,7 @@ func median(deltas []float64) float64 {
 	return deltas[len(deltas)/2]
 }
 
-// madmInt calculates the median absolute deviation of the given slice of int values
+// calculates the median absolute deviation of the given slice of int values
 func madmInt(sizes []int) float64 {
 	floatSizes := make([]float64, len(sizes))
 	for i := range sizes {
