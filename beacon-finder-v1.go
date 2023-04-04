e15297f47b18e5dda05d7082fc5290f24f53075b
Chad Pierce <chadpierce@users.noreply.github.com>
2023-04-03 20:09:02 -0500

    new scoring system, first ver renamed

--- beacon-finder-v1.go
+++ beacon-finder-v1.go
@@ -11,8 +11,6 @@ package main
  *	LIABILITY, THEN DO NOT DOWNLOAD OR USE THE SCRIPT. NO TECHNICAL SUPPORT
  *	WILL BE PROVIDED.
  *
- * 	!! TODO update all of this text w/ details on new scoring system
- *
  *	RITA-like beacon detection based on https://github.com/ppopiolek/c2-detection-using-statistical-analysis/blob/main/RITA_pcap.ipynb
  *	and https://github.com/Cyb3r-Monk/RITA-J/blob/main/C2%20Detection%20-%20HTTP.ipynb
  *
@@ -41,11 +39,6 @@ package main
  *
  */
 
-// back up files
-// clean up code
-// take normal, and beacon, make log file with only that data - run through both scripts
-// compare, tune
-
 import (
 	"encoding/csv"
 	"flag"
@@ -62,32 +55,28 @@ import (
 
 // arguments
 type Options struct {
-	Help           bool
-	InputFile      string
-	OutputFile     string
-	OutputDefault  bool
-	Comma          string
-	TimeFormat     string
-	ColumnTime     int
-	ColumnSource   int
-	ColumnDest     int
-	ColumnByteRecv int
-	ColumnByteSent int
-	MaxSources     int
-	MinScore       float64
-	MinConnCount   int
-	WeightTime     float64
-	WeightData     float64
-	WeightTSSkew   float64
-	WeightTSMadm   float64
-	WeightTSConn   float64
-	WeightDSSkew   float64
-	WeightDSMadm   float64
-	WeightDSSmall  float64
-	InputProxy     bool
-	InputDNS       bool
-	NoBytes        bool
-	Debug          bool
+	Help            bool
+	InputFile       string
+	OutputFile      string
+	OutputDefault   bool
+	Comma           string
+	TimeFormat      string
+	ColumnTime      int
+	ColumnSource    int
+	ColumnDest      int
+	ColumnByteRecv  int
+	ColumnByteSent  int
+	MaxSources      int
+	MinScore        float64
+	MinConnCount    int
+	WeightSkew      float64
+	WeightMadm      float64
+	WeightConnCount float64
+	WeightSize      float64
+	InputProxy      bool
+	InputDNS        bool
+	NoBytes         bool
+	Debug           bool
 }
 
 // represents a row in the CSV file
@@ -111,21 +100,13 @@ type GroupedRecord struct {
 
 // represents a grouped record with calculated scores
 type ScoredRecord struct {
-	Src     string
-	Dst     string
-	Score   float64
-	DSScore float64
-	TSScore float64
-	DSSkew  float64
-	DSMadm  float64
-	DSSmall float64
-	TSSkew  float64
-	TSMadm  float64
-	TSConn  float64
-	//SizeScore      float64
-	//SkewScore      float64
-	//MadmScore      float64
-	//ConnCountScore float64
+	Src            string
+	Dst            string
+	Score          float64
+	SizeScore      float64
+	SkewScore      float64
+	MadmScore      float64
+	ConnCountScore float64
 }
 
 func main() {
@@ -165,33 +146,22 @@ func main() {
 
 		// skip rows where source or destination is "-"  // TODO make this optional?
 		if row[srcCol] == "-" || row[dstCol] == "-" {
-			//fmt.Println("DEBUG dashes")
 			continue
 		}
 
-		// parse timestamp format
-		// timeFmtStr := opts.TimeFormat
-		// timestamp, err := time.Parse(timeFmtStr, row[timeCol])
-		// if err != nil {
-		// 	log.Fatal(err) // throw warning and skip line? - not sure if good idea?
-		// 	// INPROG - add prompt to continue after error?
-		// 	// otherwise an error may be thrown for every line
-		// 	//log.Println("WARNING: ", err)
-		// 	//continue
-		// }
-
-		//for testing, uses epoch time
-		//>go run beacon_finder.go -ct 0 -cs 1 -cd 2 -cx 3 -cr 4 -i http-dataset2.log -d " " -O
-		//timestampStr := "1371601525.249082"
-		timestampStr := row[timeCol]
-		secs, err := strconv.ParseFloat(timestampStr, 64)
+		// parse timestamp from first column
+		// timeFmtStr := "2006-01-02-15:04:05"
+		timeFmtStr := opts.TimeFormat
+		timestamp, err := time.Parse(timeFmtStr, row[timeCol])
 		if err != nil {
-			// handle error
+			log.Fatal(err) // throw warning and skip line? - not sure if good idea?
+			// INPROG - add prompt to continue after error?
+			// otherwise an error may be thrown for every line
+			//log.Println("WARNING: ", err)
+			//continue
 		}
-		timestamp := time.Unix(int64(secs), int64((secs-math.Floor(secs))*1e9))
 
 		// if NoBytes flag was passed, set to 0 - otherwise get values from csv
-		// TODO only consider bytes SENT!
 		var bytesSent int
 		var bytesReceived int
 		if opts.NoBytes {
@@ -247,88 +217,47 @@ func main() {
 
 		go func(groupedRecord GroupedRecord) {
 			defer wg.Done()
-			// time based scoring
 
-			tsDeltas := make([]float64, len(groupedRecord.Times)-1)
+			deltas := make([]float64, len(groupedRecord.Times)-1)
 			for i := 1; i < len(groupedRecord.Times); i++ {
-				tsDeltas[i-1] = groupedRecord.Times[i].Sub(groupedRecord.Times[i-1]).Seconds()
+				deltas[i-1] = groupedRecord.Times[i].Sub(groupedRecord.Times[i-1]).Seconds()
 			}
 
-			tsLowVal := percentile(tsDeltas, 20)
-			tsMidVal := percentile(tsDeltas, 50)
-			tsHighVal := percentile(tsDeltas, 80)
+			lowVal := percentile(deltas, 20)
+			midVal := percentile(deltas, 50)
+			highVal := percentile(deltas, 80)
 
-			tsBowleyNumVal := tsLowVal + tsHighVal - 2*tsMidVal
-			tsBowleyDenVal := tsHighVal - tsLowVal
+			bowleyNumVal := lowVal + highVal - 2*midVal
+			bowleyDenVal := highVal - lowVal
 
-			tsSkewVal := tsBowleyNumVal / tsBowleyDenVal
-			if tsBowleyNumVal == 0 || tsMidVal == tsLowVal || tsMidVal == tsHighVal {
-				tsSkewVal = 0
+			skewVal := bowleyNumVal / bowleyDenVal
+			if bowleyNumVal == 0 || midVal == lowVal || midVal == highVal {
+				skewVal = 0
+			}
+			madmVal := madm(deltas)
+
+			skewScoreVal := 1 - math.Abs(skewVal)
+			madmScoreVal := 1 - madmVal/30
+			if madmScoreVal < 0 {
+				madmScoreVal = 0
 			}
 
-			// time delta score calculation
-			tsSkewScore := 1 - math.Abs(tsSkewVal)
+			connDivVal := groupedRecord.Times[len(groupedRecord.Times)-1].Sub(groupedRecord.Times[0]).Seconds()
 
-			tsMadmVal := madmFloat(tsDeltas)
-			// If jitter is greater than 30 seconds, set madm score to 0
-			// TODO this may need tuning
-			tsMadmScore := 1 - tsMadmVal/30
-			if tsMadmScore < 0 {
-				tsMadmScore = 0
+			connCountScoreVal := 10 * float64(len(groupedRecord.Times)) / connDivVal
+
+			if connCountScoreVal > 1 {
+				connCountScoreVal = 1
 			}
 
-			// num of connections scoring
-			// TODO 90 value could use tuning?
+			sentMadm := madmInt(groupedRecord.SentSizes)
+			receivedMadm := madmInt(groupedRecord.ReceivedSizes)
 
-			fmt.Printf("DEBUG conns %v %v\n", groupedRecord.Src, len(groupedRecord.Times))
-
-			tsConnDivVal := groupedRecord.Times[len(groupedRecord.Times)-1].Sub(groupedRecord.Times[0]).Seconds() / 90
-
-			tsConnCountScore := 10 * float64(len(groupedRecord.Times)) / tsConnDivVal
-			if tsConnCountScore > 1 {
-				tsConnCountScore = 1
+			sizeScore := 1 - (sentMadm+receivedMadm)/2/1024
+			if sizeScore < 0 {
+				sizeScore = 0
 			}
 
-			// data based scoring
-			// NOTE TODO only care about data sent - maybe revisit this
-			dsSentMadm := madmInt(groupedRecord.SentSizes)
-			//receivedMadm := madmInt(groupedRecord.ReceivedSizes)
-
-			//sizeScore := 1 - (sentMadm+receivedMadm)/2/1024
-			dsSizeScore := 1 - dsSentMadm/1024
-
-			if dsSizeScore < 0 {
-				dsSizeScore = 0
-			}
-
-			dsLowVal := percentileInt(groupedRecord.SentSizes, 20.0)
-			dsMidVal := percentileInt(groupedRecord.SentSizes, 50.0)
-			dsHighVal := percentileInt(groupedRecord.SentSizes, 80.0)
-
-			dsBowleyNumVal := dsLowVal + dsHighVal - 2*dsMidVal
-			dsBowleyDenVal := dsHighVal - dsLowVal
-
-			dsSkewVal := dsBowleyNumVal / dsBowleyDenVal
-			if dsBowleyNumVal == 0 || dsMidVal == dsLowVal || dsMidVal == dsHighVal {
-				dsSkewVal = 0
-			}
-
-			dsSkewScore := 1 - math.Abs(dsSkewVal)
-
-			// if jitter over 128 bytes, score is zero. may need tuning
-			dsMadmScore := 1.0 - (dsSizeScore / 128.0)
-			if dsMadmScore < 0 {
-				dsMadmScore = 0
-			}
-			// looking for low data sent values
-			// a higher value (default 8192) is less sensitive
-			// TODO tuning
-			dsSmallnessScore := 1.0 - (dsMidVal / 4500) //8192.0)
-			if dsSmallnessScore < 0 {
-				dsSmallnessScore = 0
-			}
-
-			/* LEGACY SCORING SYSTEM
 			// weights for each sub-score
 			skewWeight := opts.WeightSkew
 			madmWeight := opts.WeightMadm
@@ -338,69 +267,21 @@ func main() {
 				sizeWeight = 0
 			}
 
-			scoreVal := (skewWeight*tsSkewScoreVal + madmWeight*tsMadmScore + connCountWeight*tsConnCountScore +
-				sizeWeight*dsSizeScore) / (skewWeight + madmWeight + connCountWeight + sizeWeight)
+			scoreVal := (skewWeight*skewScoreVal + madmWeight*madmScoreVal + connCountWeight*connCountScoreVal +
+				sizeWeight*sizeScore) / (skewWeight + madmWeight + connCountWeight + sizeWeight)
 			// unweighted scoring:
 			// scoreVal := (skewScoreVal + madmScoreVal + connCountScoreVal + sizeScore) / 4
-			*/
-
-			// weights for each sub-score
-			timeWeight := opts.WeightTime
-			dataWeight := opts.WeightData
-			tsSkewWeight := opts.WeightTSSkew
-			tsMadmWeight := opts.WeightTSMadm
-			tsConnWeight := opts.WeightTSConn
-			dsSkewWeight := opts.WeightDSSkew
-			dsMadmWeight := opts.WeightDSMadm
-			dsSmallWeight := opts.WeightDSSmall
-			if opts.NoBytes {
-				dataWeight = 0
-			}
-
-			// Final Scoring, weighted
-			tsScore := ((tsSkewWeight*tsSkewScore + tsMadmWeight*tsMadmScore + tsConnWeight*tsConnCountScore) / (tsSkewWeight + tsMadmWeight + tsConnWeight))   // * 1000) / 1000
-			dsScore := ((dsSkewWeight*dsSkewScore + dsMadmWeight*dsMadmScore + dsSmallWeight*dsSmallnessScore) / (dsSkewWeight + dsMadmWeight + dsSmallWeight)) // * 1000) / 1000
-
-			scoreVal := (timeWeight*tsScore + dataWeight*dsScore) / (timeWeight + dataWeight)
-
-			//fmt.Printf("score %v ts %v ds %v \n", scoreVal, tsScore, dsScore)
-
-			// // Final Scoring, not weighted
-			// dsScore := (((dsSkewScore + dsMadmScore + dsSmallnessScore) / 3.0) * 1000) / 1000
-			// tsScore := (((tsSkewScore + tsMadmScore + tsConnCountScore) / 3.0) * 1000) / 1000
-			// scoreVal := (dsScore + tsScore) / 2
-
-			// DEBUG
-			// testdsScore := (dsSkewScore + dsMadmScore + dsSmallnessScore) / 3.0 //) * 1000) / 1000
-			// testtsScore := (tsSkewScore + tsMadmScore + tsConnCountScore) / 3.0 //) * 1000) / 1000
-			// testscoreVal := (testdsScore + testtsScore) / 2
-			// fmt.Printf("TEST score %v ts %v ds %v \n", testscoreVal, testtsScore, testdsScore)
 
 			scoredRecord := ScoredRecord{
-				Src:     groupedRecord.Src,
-				Dst:     groupedRecord.Dst,
-				Score:   scoreVal,
-				DSScore: dsScore,
-				TSScore: tsScore,
-				DSSkew:  dsSkewScore,
-				DSMadm:  dsMadmScore,
-				DSSmall: dsSmallnessScore,
-				TSSkew:  tsSkewScore,
-				TSMadm:  tsMadmScore,
-				TSConn:  tsConnCountScore,
+				Src:            groupedRecord.Src,
+				Dst:            groupedRecord.Dst,
+				Score:          scoreVal,
+				SizeScore:      sizeScore,
+				SkewScore:      skewScoreVal,
+				MadmScore:      madmScoreVal,
+				ConnCountScore: connCountScoreVal,
 			}
 
-			// scoredRecord := ScoredRecord{
-			// 	Src:            groupedRecord.Src,
-			// 	Dst:            groupedRecord.Dst,
-			// 	Score:          scoreVal,
-			// 	SizeScore:      dsSizeScore,
-			// 	SkewScore:      tsSkewScoreVal,
-			// 	MadmScore:      tsMadmScore,
-			// 	ConnCountScore: tsConnCountScore,
-			// }
-
-			//fmt.Printf("## DEBUG: %v \n", scoredRecord)
 			// only return scored records above threshold
 			if scoreVal > opts.MinScore {
 				scores <- scoredRecord
@@ -448,19 +329,15 @@ func getOptions() Options {
 	flag.IntVar(&opts.MinConnCount, "m", 36, "minimum number of connections threshold")
 	flag.IntVar(&opts.MaxSources, "s", 5, "maximum number of sources for destination threshold")
 	flag.Float64Var(&opts.MinScore, "S", .500, "minimum score threshold")
-	flag.IntVar(&opts.ColumnTime, "cT", 0, "csv column for timestamp (default 0)")
-	flag.IntVar(&opts.ColumnSource, "cS", 2, "csv column for source")
-	flag.IntVar(&opts.ColumnDest, "cD", 7, "csv column for destination")
-	flag.IntVar(&opts.ColumnByteRecv, "cR", 11, "csv column for bytes recevied")
-	flag.IntVar(&opts.ColumnByteSent, "cX", 12, "csv column for bytes sent")
-	flag.Float64Var(&opts.WeightTime, "wT", 1.0, "weight value for overall time score")
-	flag.Float64Var(&opts.WeightData, "wD", 1.0, "weight value for overall data score")
-	flag.Float64Var(&opts.WeightTSSkew, "wTS", 1.0, "weight value for time skew score")
-	flag.Float64Var(&opts.WeightTSMadm, "wTM", 1.0, "weight value for time MADM score")
-	flag.Float64Var(&opts.WeightTSConn, "wTC", 1.0, "weight value time connection count score")
-	flag.Float64Var(&opts.WeightDSSkew, "wDS", 1.0, "weight value for data size skew score")
-	flag.Float64Var(&opts.WeightDSMadm, "wDM", 1.0, "weight value for data MADM score")
-	flag.Float64Var(&opts.WeightDSSmall, "wDZ", 1.0, "weight value for data smallness score")
+	flag.IntVar(&opts.ColumnTime, "ct", 0, "csv column for timestamp (default 0)")
+	flag.IntVar(&opts.ColumnSource, "cs", 2, "csv column for source")
+	flag.IntVar(&opts.ColumnDest, "cd", 7, "csv column for destination")
+	flag.IntVar(&opts.ColumnByteRecv, "cr", 11, "csv column for bytes recevied")
+	flag.IntVar(&opts.ColumnByteSent, "cx", 12, "csv column for bytes sent")
+	flag.Float64Var(&opts.WeightSkew, "ws", 1.0, "weight value for skew score")
+	flag.Float64Var(&opts.WeightMadm, "wm", 1.0, "weight value for MADM score")
+	flag.Float64Var(&opts.WeightConnCount, "wc", 1.0, "weight value connection count score")
+	flag.Float64Var(&opts.WeightSize, "wz", 1.0, "weight value for data size score")
 	flag.BoolVar(&opts.InputProxy, "P", false, "use Proxy Log CSV Inputs")
 	flag.BoolVar(&opts.InputDNS, "D", false, "use DNS Log CSV Inputs (no size analysis)")
 	flag.BoolVar(&opts.NoBytes, "B", false, "do not use bytes sent/received in analysis")
@@ -498,19 +375,19 @@ func getOptions() Options {
 		os.Exit(0)
 	} else if opts.InputProxy && !opts.InputDNS {
 		log.Println("INFO: Proxy mode selected")
-		if !isFlagPassed("cT") {
+		if !isFlagPassed("ct") {
 			opts.ColumnTime = 0
 		}
-		if !isFlagPassed("cS") {
+		if !isFlagPassed("cs") {
 			opts.ColumnSource = 2
 		}
-		if !isFlagPassed("cD") {
+		if !isFlagPassed("cd") {
 			opts.ColumnDest = 7
 		}
-		if !isFlagPassed("cX") {
+		if !isFlagPassed("cx") {
 			opts.ColumnByteSent = 11
 		}
-		if !isFlagPassed("cR") {
+		if !isFlagPassed("cr") {
 			opts.ColumnByteRecv = 12
 		}
 		if !isFlagPassed("d") {
@@ -521,23 +398,23 @@ func getOptions() Options {
 		}
 	} else if !opts.InputProxy && opts.InputDNS {
 		log.Println("INFO: DNS mode selected")
-		if !isFlagPassed("cT") {
+		if !isFlagPassed("ct") {
 			opts.ColumnTime = 0
 		}
-		if !isFlagPassed("cS") {
+		if !isFlagPassed("cs") {
 			opts.ColumnSource = 1
 		}
-		if !isFlagPassed("cD") {
+		if !isFlagPassed("cd") {
 			opts.ColumnDest = 2
 		}
-		if !isFlagPassed("cX") {
+		if !isFlagPassed("cx") {
 			opts.ColumnByteSent = -1
 		}
-		if !isFlagPassed("cR") {
+		if !isFlagPassed("cr") {
 			opts.ColumnByteRecv = -1
 		}
-		if !isFlagPassed("wD") {
-			opts.WeightData = 0
+		if !isFlagPassed("wz") {
+			opts.WeightSize = 0
 		}
 		if !isFlagPassed("B") {
 			opts.NoBytes = true
@@ -565,18 +442,13 @@ func writeOutput(scoredRecords []ScoredRecord, outputFile string, noBytes bool) 
 	for _, scoredRecord := range scoredRecords {
 		var output string
 		if noBytes {
-			// output = fmt.Sprintf("%s -> %s | SCORE: %.3f | (skew: %.3f) (madm: %.3f) (connCount: %.3f) (size: -)\n",
-			// 	scoredRecord.Src, scoredRecord.Dst, scoredRecord.Score, scoredRecord.SkewScore, scoredRecord.MadmScore,
-			// 	scoredRecord.ConnCountScore)
-			output = fmt.Sprintf("%s -> %s | score: %.3f | (ts: %.3f ds: -) | (tsSkew: %.3f tsMadm: %.3f tsConn: %.3f) (dsSkew: - dsMadm: - dsSmallness: -)\n",
-				scoredRecord.Src, scoredRecord.Dst, scoredRecord.Score, scoredRecord.TSScore, scoredRecord.TSSkew, scoredRecord.TSMadm, scoredRecord.TSConn)
+			output = fmt.Sprintf("%s -> %s | SCORE: %.3f | (skew: %.3f) (madm: %.3f) (connCount: %.3f) (size: -)\n",
+				scoredRecord.Src, scoredRecord.Dst, scoredRecord.Score, scoredRecord.SkewScore, scoredRecord.MadmScore,
+				scoredRecord.ConnCountScore)
 		} else {
-			// output = fmt.Sprintf("%s -> %s | SCORE: %.3f | (skew: %.3f) (madm: %.3f) (connCount: %.3f) (size: %.3f)\n",
-			// 	scoredRecord.Src, scoredRecord.Dst, scoredRecord.Score, scoredRecord.SkewScore, scoredRecord.MadmScore,
-			// 	scoredRecord.ConnCountScore, scoredRecord.SizeScore)
-			output = fmt.Sprintf("%s -> %s | score: %.3f | (ts: %.3f ds: %.3f) | (tsSkew: %.3f tsMadm: %.3f tsConn: %.3f) (dsSkew: %.3f dsMadm: %.3f dsSmallness: %.3f)\n",
-				scoredRecord.Src, scoredRecord.Dst, scoredRecord.Score, scoredRecord.TSScore, scoredRecord.DSScore, scoredRecord.TSSkew, scoredRecord.TSMadm,
-				scoredRecord.TSConn, scoredRecord.DSSkew, scoredRecord.DSMadm, scoredRecord.DSSmall)
+			output = fmt.Sprintf("%s -> %s | SCORE: %.3f | (skew: %.3f) (madm: %.3f) (connCount: %.3f) (size: %.3f)\n",
+				scoredRecord.Src, scoredRecord.Dst, scoredRecord.Score, scoredRecord.SkewScore, scoredRecord.MadmScore,
+				scoredRecord.ConnCountScore, scoredRecord.SizeScore)
 		}
 		// print to file if output filename exists, or print to console
 		if outputFile != "" {
@@ -697,18 +569,8 @@ func percentile(deltas []float64, p float64) float64 {
 	return deltas[int(index)]
 }
 
-func percentileInt(sizes []int, p float64) float64 {
-	floatSizes := make([]float64, len(sizes))
-	sort.Float64s(floatSizes)
-	index := p / 100.0 * float64(len(floatSizes))
-	if index == float64(int(index)) {
-		return (floatSizes[int(index)-1] + floatSizes[int(index)]) / 2
-	}
-	return floatSizes[int(index)]
-}
-
 // calculates the median absolute deviation of the given slice of float64 values
-func madmFloat(deltas []float64) float64 {
+func madm(deltas []float64) float64 {
 	medianDelta := median(deltas)
 	absDeltas := make([]float64, len(deltas))
 	for i := range deltas {
@@ -732,5 +594,5 @@ func madmInt(sizes []int) float64 {
 	for i := range sizes {
 		floatSizes[i] = float64(sizes[i])
 	}
-	return madmFloat(floatSizes)
+	return madm(floatSizes)
 }
